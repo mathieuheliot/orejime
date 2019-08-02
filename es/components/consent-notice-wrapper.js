daaafab63eb282bf022dbc7c567f8246faa5f413
Mathieu Héliot <mathieu.heliot@gmail.com>
2019-08-02 22:47:07 +0200

    fix : Wrong class name

--- es/components/consent-notice-wrapper.js
+++ es/components/consent-notice-wrapper.js
@@ -48,7 +48,7 @@ function (_React$Component) {
           overlayClassName: isVisible ? 'modal modal--drawer visible' : 'modal modal--drawer',
           className: "drawer"
         }), React.createElement("div", {
-          class: "drawer__body"
+          class: "drawer__content"
         }, React.createElement(ConsentNotice, props)));
       }
 
